5fb9767cf91b701b3ca701c5e31a203af7825dc5
Hack Reactor Students <accounts@hackreactor.com>
2017-02-25 11:49:13 -0800

    Should dynamically compute the youtube url

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,24 @@
 angular.module('video-player', [])
-.controller('appController', function() {
+.controller('appController', ['$sce', function($sce) {
   this.videoData = window.exampleVideoData;
-})
-.directive('app', function() {
+  this.video = window.exampleVideoData[0];
+}])
+.directive('app', function($sce) {
   return {
     controller: 'appController',
     controllerAs: 'ctrl',
     bindToController: true
   };
 });
+
+// angular.module('mySceApp', ['ngSanitize'])
+// .controller('AppController', ['$http', '$templateCache', '$sce',
+//   function AppController($http, $templateCache, $sce) {
+//     var self = this;
+//     $http.get('test_data.json', {cache: $templateCache}).then(function(response) {
+//       self.userComments = response.data;
+//     });
+//     self.explicitlyTrustedHtml = $sce.trustAsHtml(
+//         '<span onmouseover="this.textContent=&quot;Explicitly trusted HTML bypasses ' +
+//         'sanitization.&quot;">Hover over this text.</span>');
+//   }]);
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,24 +1,48 @@
 angular.module('video-player')
-.controller('videoPlayerController', function() {
-})
+.controller('videoPlayerController', [ '$sce', function($sce) {
+  console.log($sce);
 
-.directive('videoPlayer', function() {
+  // this.videoIdNumber = video.id.videoId;
+  // console.log(this.videoIdNumber);
+}])
+
+.directive('videoPlayer', function($sce) {
   return {
     scope: {
-      videos: '<'
+      video: '<'
     },
     controller: 'videoPlayerController',
     controllerAs: 'ctrl',
     bindToController: true,
-    template:
-      '<div class="video-player"> \
-        <div class="embed-responsive embed-responsive-16by9"> \
-          <iframe class="embed-responsive-item" ng-src="https://www.youtube.com/embed/" + "{{ctrl.videos[0].id.videoId}}" + "?autoplay=0" allowFullScreen></iframe> \
-        </div> \
-        <div class="video-player-details"> \
-          <h3>{{ctrl.videos[0].snippet.title}}</h3> \
-          <div>{{ctrl.videos[0].snippet.description}}</div> \
-        </div> \
-      </div>'
+    controller: function() {
+      this.url = function(videoId) {
+        return $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + videoId);
+
+        // $sce.trustAsResourceUrl('https://www.youtube.com/embed/{{ctrl.video.id.videoId}}');
+      };
+    },
+    templateUrl: 'src/templates/videoPlayer.html'
   };
 });
+    // template:
+    //   '<div class="video-player"> \
+    //     <div class="embed-responsive embed-responsive-16by9"> \
+    //       <iframe class="embed-responsive-item" ng-src="https://www.youtube.com/embed/" + "{{ctrl.videos[0].id.videoId}}" + "?autoplay=0" allowFullScreen></iframe> \
+    //     </div> \
+    //     <div class="video-player-details"> \
+    //       <h3>{{ctrl.videos[0].snippet.title}}</h3> \
+    //       <div>{{ctrl.videos[0].snippet.description}}</div> \
+    //     </div> \
+    //   </div>'
+
+// angular.module('mySceApp', ['ngSanitize'])
+// .controller('AppController', ['$http', '$templateCache', '$sce',
+//   function AppController($http, $templateCache, $sce) {
+//     var self = this;
+//     $http.get('test_data.json', {cache: $templateCache}).then(function(response) {
+//       self.userComments = response.data;
+//     });
+//     self.explicitlyTrustedHtml = $sce.trustAsHtml(
+//         '<span onmouseover="this.textContent=&quot;Explicitly trusted HTML bypasses ' +
+//         'sanitization.&quot;">Hover over this text.</span>');
+//   }]);
